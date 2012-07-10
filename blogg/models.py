946b6809daf510e7c328b92ee3bead63dd168ff0
Mensah Adjei <nbadjei@gmail.com>
2012-07-10 11:34:21 +0000

    bloggy

--- blogg/models.py
+++ blogg/models.py
@@ -5,18 +5,21 @@ from django.contrib import admin
 
 class Post(models.Model):
     title = models.CharField(max_length=60)
-    P_body = models.TextField()
-    P_created = models.DateField()
-    P_updated = models.DateField(auto_now=True)
+    body = models.TextField()
+    created = models.DateField()
+    updated = models.DateField(auto_now=True)
     def __unicode__(self):
         return self.title
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
-    C_body = models.TextField()
-    C_created = models.DateField()
-    C_updated = models.DateField(auto_now=True)
-    post = models.ForeignKey(Post)
+    body = models.TextField()
+    created = models.DateField()
+    updated = models.DateField(auto_now=True)
+    post = models.ForeignKey(Post, related_name = 'comments')
+    def __unicode__(self):
+        return self.author
+    
     def first_sixty(self):
         return self.body[:60]
         
@@ -24,14 +27,14 @@ class CommentInline(admin.TabularInline):
     model = Comment 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'P_created', 'P_updated')
-    search_fields = ('title', 'P_body')
-    list_filter = ('P_created', 'P_updated')
+    list_display = ('title', 'created', 'updated')
+    search_fields = ('title', 'body')
+    list_filter = ('created', 'updated')
     inlines = [CommentInline]   
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('post', 'author', 'first_sixty','C_created', 'C_updated' )
-    list_filter = ('C_created', 'author')
+    list_display = ('post', 'author', 'first_sixty','created', 'updated' )
+    list_filter = ('created', 'author')
     inlines = [CommentInline]    
 
 admin.site.register(Post, PostAdmin)
--- blogg/views.py
+++ blogg/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.template import Context, loader
 from django.http import HttpResponse
-
+import re
 from models import Post, Comment 
 
 
@@ -15,14 +15,21 @@ def post_list(request):
 
 def post_detail(request, id, showComments=False):
     single_post = Post.objects.get(pk=id)
+    html = '<h3>'+str(single_post) + '</h3><br/>' + str(single_post.body)  + '<br/><h5>Comments</h5><br/>'
+    comm = ' '
+    for i in single_post.comments.all():
+        comm += i.body + '<br/>'
     return HttpResponse(single_post)
 
 def post_search(request, term):
-    pass
-  #  searched = Post.objects.value(term)
-    #return HttpReponse(searched)
+    searched = Post.objects.filter(body__contains=term)
+    res = ' '
+    for i in searched:
+        res +=  i.title + ' : ' + i.body
+        
+    return HttpResponse(res)
 
 def home(request):
     print 'it works'
-    return HttpResponse('hello world. Ete zene?') 
+    return HttpResponse('hellurrrr world. Ete zene?') 
 
