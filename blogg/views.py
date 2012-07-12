c90243b0f7e924017a6b030caa8bca14b9e64578
Mensah Adjei <nbadjei@gmail.com>
2012-07-12 17:10:14 +0000

    bloggy

--- blogg/views.py
+++ blogg/views.py
@@ -2,7 +2,10 @@
 from django.template import Context, loader
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-from models import Post, Comment 
+from models import Post, Comment
+from django.forms import ModelForm
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
 
 '''
 def post_list(request):
@@ -19,14 +22,28 @@ def post_list(request):
     c = Context({'posts':posts})
     return HttpResponse(t.render(c))
 
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        exclude=['post']
+        
+@csrf_exempt
 def post_detail(request, id, showComments=False):
+    if request.method =="POST":
+        comment = Comment(post=target_post)
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect(request.path)
+    else:
+        form =CommentForm()    
     posts = Post.objects.get(pk=id)
     comments = posts.comments.all
     '''   html = '<h3>'+str(single_post) + '</h3><br/>' + str(single_post.body)  + '<br/><h5>Comments</h5><br/>'
     comm = ' '
     for i in single_post.comments.all():
         comm += i.body + '<br/>'''
-    return render_to_response('blogg/post_detail.html', {'posts':posts, 'comments':comments})
+    return render_to_response('blogg/post_detail.html', {'posts':posts, 'comments':comments, 'form':form})
 
 def post_search(request, term):
     posts = Post.objects.filter(body__contains=term) 
